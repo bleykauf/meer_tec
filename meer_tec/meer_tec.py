74a7290f10773f6f4418e31017b1ac2ba8e458c8
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2023-07-24 10:40:03 +0200

    more typehints

--- meer_tec/meer_tec.py
+++ meer_tec/meer_tec.py
@@ -2,6 +2,7 @@ import random
 import socket
 import struct
 import time
+from typing import Optional, Union
 
 from PyCRC.CRCCCITT import CRCCCITT as CRC
 
@@ -25,6 +26,12 @@ def _id_to_hex(cmd_id: int) -> str:
 def _calc_checksum(string: str) -> str:
     """Calculate CRC checksum."""
     return "{:04X}".format(CRC().calculate(string))
+
+
+def _generate_request_number():
+    # generate a random request number
+    num = random.randint(0, 65535)
+    return "{:04X}".format(num)
 
 
 class XPort(socket.socket):
@@ -54,7 +61,7 @@ class TEC:
 
     def get_parameter(
         self, cmd_id: int, value_type, request_number=None, instance: int = 1
-    ):
+    ) -> Union[float, int]:
         cmd = "?VR" + _id_to_hex(cmd_id) + "{:02X}".format(instance)
         request = Request(cmd, self.addr, request_number=request_number)
         reponse = Response(self._tcpip_query(request), request, value_type=value_type)
@@ -62,7 +69,7 @@ class TEC:
 
     def set_parameter(
         self, cmd_id: int, value, value_type, request_number=None, instance: int = 1
-    ):
+    ) -> None:
         cmd = "VS" + _id_to_hex(cmd_id) + "{:02X}".format(instance)
         if value_type is float:
             cmd += _float_to_hex(value)
@@ -72,7 +79,7 @@ class TEC:
         reponse = Response(self._tcpip_query(request), request, value_type=value_type)
         print(reponse)
 
-    def reset(self):
+    def reset(self) -> None:
         # FIXME: Could not find this in the documentation
         # self.set_parameter("?RS", value_type=int)
         raise NotImplementedError
@@ -80,39 +87,39 @@ class TEC:
     # Common product parameters
 
     @property
-    def device_value_typee(self):
+    def device_value_typee(self) -> int:
         """
         Device value_typee.
 
         1122 → TEC-1122
         """
-        return self.get_parameter(100, value_type=int)
+        return self.get_parameter(100, value_type=int)  # type: ignore[return-value]
 
     @property
-    def hardware_version(self):
+    def hardware_version(self) -> int:
         """
         Hardware Version.
 
         123 → 1.23
         """
-        return self.get_parameter(101, value_type=int)
+        return self.get_parameter(101, value_type=int)  # type: ignore[return-value]
 
     @property
-    def serial_number(self):
+    def serial_number(self) -> int:
         """Serial Number."""
-        return self.get_parameter(102, value_type=int)
+        return self.get_parameter(102, value_type=int)  # type: ignore[return-value]
 
     @property
-    def firmware_version(self):
+    def firmware_version(self) -> int:
         """
         Firmware Version.
 
         123 → 1.23
         """
-        return self.get_parameter(103, value_type=int)
+        return self.get_parameter(103, value_type=int)  # type: ignore[return-value]
 
     @property
-    def device_status(self):
+    def device_status(self) -> int:
         """
         Device Status.
 
@@ -123,35 +130,35 @@ class TEC:
         4: Bootloader
         5: Device will Reset within next 200ms
         """
-        return self.get_parameter(104, value_type=int)
+        return self.get_parameter(104, value_type=int)  # type: ignore[return-value]
 
     @property
-    def error_number(self):
+    def error_number(self) -> int:
         """Error number."""
-        return self.get_parameter(105, value_type=int)
+        return self.get_parameter(105, value_type=int)  # type: ignore[return-value]
 
     @property
-    def error_instance(self):
+    def error_instance(self) -> int:
         """Error Instance."""
-        return self.get_parameter(106, value_type=int)
+        return self.get_parameter(106, value_type=int)  # type: ignore[return-value]
 
     @property
-    def error_parameter(self):
+    def error_parameter(self) -> int:
         """Error Parameter."""
-        return self.get_parameter(107, value_type=int)
+        return self.get_parameter(107, value_type=int)  # type: ignore[return-value]
 
     @property
-    def save_data_to_flash(self):
+    def save_data_to_flash(self) -> int:
         """
         Save Data to Flash.
 
         0: Enabled
         1: Disabled (All Parameters can then be used as RAM Parameters)
         """
-        return self.get_parameter(108, value_type=int)
+        return self.get_parameter(108, value_type=int)  # type: ignore[return-value]
 
     @property
-    def parameter_system_flash_status_ro(self):
+    def parameter_system_flash_status_ro(self) -> int:
         """
         Parameter System: Flash Status (Read only).
 
@@ -160,49 +167,49 @@ class TEC:
            device now)
         2: Saving to Flash is disabled
         """
-        return self.get_parameter(109, value_type=int)
+        return self.get_parameter(109, value_type=int)  # type: ignore[return-value]
 
     # Tab: Monitor (Read only)
 
     @property
-    def object_temperature(self):
+    def object_temperature(self) -> float:
         """Object Temperature."""
         return self.get_parameter(1000, value_type=float)
 
     @property
-    def sink_temperature(self):
+    def sink_temperature(self) -> float:
         """Sink Temperature."""
         return self.get_parameter(1001, value_type=float)
 
     @property
-    def target_object_temperature_ro(self):
+    def target_object_temperature_ro(self) -> float:
         """Target Object Temperature (read-only)."""
         return self.get_parameter(1010, value_type=float)
 
     @property
-    def nominal_temperature(self):
+    def nominal_temperature(self) -> float:
         """(Ramp) Nominal Object Temperature."""
         return self.get_parameter(1011, value_type=float)
 
     @property
-    def thermal_power_model_current(self):
+    def thermal_power_model_current(self) -> float:
         """Thermal Power Model Current."""
         return self.get_parameter(1012, value_type=float)
 
     @property
-    def actual_output_current(self):
+    def actual_output_current(self) -> float:
         """Actual Output Current."""
         return self.get_parameter(1020, value_type=float)
 
     @property
-    def actual_output_voltage(self):
+    def actual_output_voltage(self) -> float:
         """Actual Output Voltage."""
         return self.get_parameter(1021, value_type=float)
 
     # Driver Status
 
     @property
-    def driver_status(self):
+    def driver_status(self) -> int:
         """
         Driver Status.
 
@@ -213,12 +220,12 @@ class TEC:
         4: Bootloader
         5: Device will Reset within the next 200ms
         """
-        return self.get_parameter(1080, value_type=int)
+        return self.get_parameter(1080, value_type=int)  # type: ignore[return-value]
 
     # Object Temperature Stability Detection
 
     @property
-    def is_stable(self):
+    def is_stable(self) -> int:
         """
         Temperature is Stable.
 
@@ -226,14 +233,14 @@ class TEC:
         1: Is not stable
         2: Is stable
         """
-        return self.get_parameter(1200, value_type=int)
+        return self.get_parameter(1200, value_type=int)  # type: ignore[return-value]
 
     # Tab: Operation
 
     # CHx Output Stage Enable
 
     @property
-    def status(self):
+    def status(self) -> int:
         """
         Status.
 
@@ -242,85 +249,85 @@ class TEC:
         2: Live OFF/ON (See ID 50000)
         3: HW Enable (Check GPIO Config)
         """
-        return self.get_parameter(2010, value_type=int)
+        return self.get_parameter(2010, value_type=int)  # type: ignore[return-value]
 
     @status.setter
-    def status(self, value):
-        self.set_parameter(2010, value, value_type=int)
+    def status(self, value) -> None:
+        self.set_parameter(2010, value, value_type=int)  # type: ignore[return-value]
 
     # Tab: Temperature Control
 
     # CHx Nominal temperature
 
     @property
-    def target_object_temperature(self):
+    def target_object_temperature(self) -> float:
         """Target Object Temperature."""
         return self.get_parameter(3000, value_type=float)
 
     @target_object_temperature.setter
-    def target_object_temperature(self, value):
+    def target_object_temperature(self, value) -> None:
         self.set_parameter(3000, value, value_type=float)
 
     @property
-    def coarse_temp_ramp(self):
+    def coarse_temp_ramp(self) -> float:
         """Coarse Temp Ramp."""
         return self.get_parameter(3003, value_type=float)
 
     @coarse_temp_ramp.setter
-    def coarse_temp_ramp(self, value):
+    def coarse_temp_ramp(self, value) -> None:
         self.set_parameter(3003, value, value_type=float)
 
     @property
-    def proximity_width(self):
+    def proximity_width(self) -> float:
         """Proximity Width."""
         return self.get_parameter(3002, value_type=float)
 
     @proximity_width.setter
-    def proximity_width(self, value):
+    def proximity_width(self, value) -> None:
         self.set_parameter(3002, value, value_type=float)
 
     # CHx Temperature Controller PID Values
 
     @property
-    def kp(self):
+    def kp(self) -> float:
         """Kp."""
         return self.get_parameter(3010, value_type=float)
 
     @kp.setter
-    def kp(self, value):
+    def kp(self, value) -> None:
         self.set_parameter(3010, value, value_type=float)
 
     @property
-    def ti(self):
+    def ti(self) -> float:
         """Ti."""
         return self.get_parameter(3011, value_type=float)
 
     @ti.setter
-    def ti(self, value):
+    def ti(self, value) -> None:
         self.set_parameter(3011, value, value_type=float)
 
     @property
-    def td(self):
+    def td(self) -> float:
         """Td."""
         return self.get_parameter(3012, value_type=float)
 
     @td.setter
-    def td(self, value):
+    def td(self, value) -> None:
         self.set_parameter(3012, value, value_type=float)
 
     @property
-    def d_part_damping_pt1(self):
+    def d_part_damping_pt1(self) -> float:
         """D Part Damping PT1."""
         return self.get_parameter(3013, value_type=float)
 
     @d_part_damping_pt1.setter
-    def d_part_damping_pt1(self, value):
+    def d_part_damping_pt1(self, value) -> None:
         self.set_parameter(3013, value, value_type=float)
 
     # CHx Modelization for Thermal Power Regulation
 
     @property
-    def mode(self):
+    def mode(self) -> int:
         """
         Mode.
 
@@ -328,56 +335,56 @@ class TEC:
         1: Peltier, Heat Only - Cool Only
         2: Resistor, Heat Only
         """
-        return self.get_parameter(3020, value_type=int)
+        return self.get_parameter(3020, value_type=int)  # type: ignore[return-value]
 
     @mode.setter
-    def mode(self, value):
-        self.set_parameter(3020, value, value_type=int)
+    def mode(self, value) -> None:
+        self.set_parameter(3020, value, value_type=int)  # type: ignore[return-value]
 
     # CHx Peltier Characteristics
 
     @property
-    def maximal_current_imax(self):
+    def maximal_current_imax(self) -> float:
         """Maximal Current Imax."""
         return self.get_parameter(3030, value_type=float)
 
     @maximal_current_imax.setter
-    def maximal_current_imax(self, value):
+    def maximal_current_imax(self, value) -> None:
         self.set_parameter(3030, value, value_type=float)
 
     @property
-    def delta_temperature_dtmax(self):
+    def delta_temperature_dtmax(self) -> float:
         """Delta Temperature dTmax."""
         return self.get_parameter(3033, value_type=float)
 
     @delta_temperature_dtmax.setter
-    def delta_temperature_dtmax(self, value):
+    def delta_temperature_dtmax(self, value) -> None:
         self.set_parameter(3033, value, value_type=float)
 
     @property
-    def positive_current_is(self):
+    def positive_current_is(self) -> int:
         """
         Positive Current is.
 
         0: Cooling
         1: Heating
         """
-        return self.get_parameter(3034, value_type=int)
+        return self.get_parameter(3034, value_type=int)  # type: ignore[return-value]
 
     @positive_current_is.setter
-    def positive_current_is(self, value):
+    def positive_current_is(self, value) -> None:
         self.set_parameter(3034, value, value_type=int)
 
 
 class Request(str):
-    def __new__(cls, cmd, addr, request_number=None):
+    def __new__(cls, cmd: str, addr: str, request_number: Optional[str] = None):
         string = "#"
         # padding of address
         addr = "{:02d}".format(int(addr))
         if not request_number:
-            request_number = cls._generate_request_number(cls)
+            request_number = _generate_request_number()
         # stitch everything together and add checksum
-        string += addr + request_number + cmd
+        string += addr + request_number + cmd  # type: ignore[operator]
         checksum = _calc_checksum(string)
         string += checksum + "\r"
         return super().__new__(cls, string)
@@ -387,11 +394,6 @@ class Request(str):
         self.request_number = self[3:7]
         self.payload = self[7:-5]
         self.checksum = self[-5:-1]
-
-    def _generate_request_number(self):
-        # generate a random request number
-        num = random.randint(0, 65535)
-        return "{:04X}".format(num)
 
 
 class Response(str):
@@ -407,13 +409,13 @@ class Response(str):
         self.checksum = self[-5:-1]
 
     @property
-    def is_valid(self):
+    def is_valid(self) -> bool:
         checksum_correct = self.checksum == _calc_checksum(self[0:-5])
         request_match = self.request_number == self.request.request_number
         return checksum_correct & request_match
 
     @property
-    def value(self):
+    def value(self) -> Union[float, int]:
         if self.is_valid:
             if self.value_type is int:
                 return int(self.payload, 16)
